e2d80dce4c97d6c449f0610f1ca83024cb5ac612
volovar <mike@volovar.com>
2016-08-19 11:28:24 -0700

    MORE typos

--- bots/tilebot.js
+++ bots/tilebot.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
   var botPayload = {
-    text: ":argyle::argyle::argyle::argyle:\n:argyle::argle::argyle::argyle:\n:argyle::argle::argyle::argyle:"
+    text: ":argyle::argyle::argyle::argyle:\n:argyle::argyle::argyle::argyle:\n:argyle::argyle::argyle::argyle:"
   };
 
   return res.status(200).json(botPayload);
