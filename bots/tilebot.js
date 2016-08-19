df7319b05be25a8afb9363f3b53825dd359b5d34
volovar <mike@volovar.com>
2016-08-19 11:32:39 -0700

    added channel_id to tilebot

--- bots/tilebot.js
+++ bots/tilebot.js
@@ -3,5 +3,7 @@ module.exports = function (req, res, next) {
     text: ":argyle::argyle::argyle::argyle:\n:argyle::argyle::argyle::argyle:\n:argyle::argyle::argyle::argyle:"
   };
 
+  botPayload.channel = req.body.channel_id;
+
   return res.status(200).json(botPayload);
 }
