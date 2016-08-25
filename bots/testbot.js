cba2009c0c1e8a9e027d44c6e20701ab9e9e9c8e
volovar <mike@volovar.com>
2016-08-24 19:56:04 -0700

    testbot testing

--- bots/testbot.js
+++ bots/testbot.js
@@ -3,7 +3,7 @@ var Bot = require("./Bot");
 module.exports = function (req, res, next) {
   var settings = {
     token: process.env.TESTBOT_TOKEN || "Hu6C4VYZizX4bUy8Xl8Dr3I8",
-    teamId: process.env.TEAM_ID,
+    teamId: process.env.TEAM_ID || "T09Q15U78",
     botPayload: {
       username: "testbot",
       text: "Hi, I'm testbot!"
