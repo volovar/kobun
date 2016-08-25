35a2f2b2db1d79690294d06ff78639a584fe1538
volovar <mike@volovar.com>
2016-08-24 21:04:52 -0700

    rollbot fixed

--- bots/dicebot.js
+++ bots/dicebot.js
@@ -11,7 +11,31 @@ module.exports = function (req, res, next) {
   };
 
   var rollBot = new Bot(settings);
-  rollBot.botPayload.text = rollBot.createText();
+
+  var matches;
+  var times = 2;
+  var die = 6;
+  var rolls = [];
+  var total = 0;
+
+  if (req.body.text) {
+    matches = req.body.text.match(/^(\d{1,2})\s*d\s*(\d{1,3})$/);
+
+    if (matches && matches[1] && matches[2]) {
+      times = matches[1];
+      die = matches[2];
+    } else {
+      return res.status(200).send('<number>d<sides>');
+    }
+  }
+
+  for (var i = 0; i < times; i++) {
+    var currentRoll = roll(1, die);
+    rolls.push(currentRoll);
+    total += currentRoll;
+  }
+
+  rollBot.botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' + rolls.join(' + ') + ' = *' + total + '*';
 
   rollBot.send(function (error, status, body) {
     if (error) {
@@ -23,34 +47,7 @@ module.exports = function (req, res, next) {
     }
   });
 
-  rollBot.roll = function (min, max) {
+  function roll (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-  rollBot.createText = function () {
-    var matches;
-    var times = 2;
-    var die = 6;
-    var rolls = [];
-    var total = 0;
-
-    if (req.body.text) {
-      matches = req.body.text.match(/^(\d{1,2})\s*d\s*(\d{1,3})$/);
-
-      if (matches && matches[1] && matches[2]) {
-        times = matches[1];
-        die = matches[2];
-      } else {
-        return res.status(200).send('<number>d<sides>');
-      }
-    }
-
-    for (var i = 0; i < times; i++) {
-      var currentRoll = this.roll(1, die);
-      rolls.push(currentRoll);
-      total += currentRoll;
-    }
-
-    return req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' + rolls.join(' + ') + ' = *' + total + '*';
   };
 };
